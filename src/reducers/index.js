8d2cf6faa750a6842fb829ff186fba62c44576b5
aburgan <44406493+4ortytwo@users.noreply.github.com>
2019-10-27 23:05:20 +0100

    back it up

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import counterReducer from "./counter";
+import loggedReducer from "./isLogged";
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({
+  counter: counterReducer,
+  isLogged: loggedReducer
+});
+
+export default rootReducer;
